0e2c9e596b5890c2b51262e058d0a3eefcd3006b
sakib <sakibhasan94470@gmail.com>
2023-12-13 00:20:27 +0600

    Assignment  two compleate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React from "react";
+import Counter from "./components/Counter";
 
-const App = () => {
+function App() {
   return (
     <div>
       <Counter />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Counter = () => {
+function Counter() {
+  const [count, setState] = useState(0);
+  const countHandler = () => {
+    setState(count + 1);
+  };
+  const decraseHandler = () => {
+    setState(count - 1);
+  };
+  const resetHandler = () => {
+    setState(0);
+  };
   return (
-    <div className="counter center">
-      <h1 className="counter__title">Counter App</h1>
-      <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
-        <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+    <div>
+      <div className="counter center">
+        <h1 className="counter__title">Counter App</h1>
+        <div className="card center">
+          <h2 className="card__title">Count : 0{count}</h2>
+          <div className="card__btns">
+            <button onClick={countHandler} className="btn card__btn" disabled={count===5}>
+              +
+            </button>
+            <button onClick={decraseHandler} className="btn card__btn" disabled={count===-5}>
+              -
+            </button>
+            <button onClick={resetHandler} className="btn card__btn">
+              0
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Counter;
